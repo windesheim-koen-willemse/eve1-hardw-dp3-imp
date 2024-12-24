283acef63def74703e32701a92d813e626fb68cb
Koen Willemse <s1212119@student.windesheim.nl>
2024-12-24 09:46:10 +0100

    feat: added buttons

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from pyfirmata2 import Arduino
-from util import STATUS_MID
 from tft import tft_update
+from util import get_register_pin, STATUS_CALM, STATUS_MID, STATUS_BUSY
 
+# Globals
+statuses = [STATUS_CALM, STATUS_MID, STATUS_BUSY]
+num = 0
+
+# Caches
+cache_num = None
+
+# Config
 board = Arduino(Arduino.AUTODETECT)
+board.samplingOn(10)
 
-tft_update(board, STATUS_MID, "7m")
+def on_inc(on):
+    global num
+    if not on: return
+    num += 1
+inc_pin = get_register_pin(board, 'd:2:i', on_inc)
 
+def on_dec(on):
+    global num
+    if not on: return
+    num -= 1
+dec_pin = get_register_pin(board, 'd:3:i', on_dec)
+
+# Main
 while True:
-    pass
+    if cache_num == num: continue
+    cache_num = num
+    tft_update(board, statuses[num%len(statuses)], "{} per.".format(num))
--- util.py
+++ util.py
@@ -4,3 +4,9 @@ OK = False
 STATUS_CALM = 0
 STATUS_MID = 1
 STATUS_BUSY = 2
+
+def get_register_pin(board, pin, callback):
+    pin = board.get_pin(pin)
+    pin.register_callback(callback)
+    pin.enable_reporting()
+    return pin
