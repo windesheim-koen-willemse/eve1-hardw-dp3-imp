837b49aa83ac3cf27d2ad35f1a3f1ec377ae047b
Koen Willemse <s1212119@student.windesheim.nl>
2024-12-31 11:03:29 +0100

    feat: use fixed processing rate instead of one based on the history. This is more testable and is easier to update when up/downscaling

--- model.py
+++ model.py
@@ -4,30 +4,35 @@ from ui import render
 
 # CONSTANTS
 
-MAX_HISTORY_LENGTH = 255
+MAX_RECORD_LIVING_TIME_IN_SEC = 3600 # 1 hour
+M = 10.5
 
 CHANGE_ENTER = 0
 CHANGE_LEAVE = 1
 
 # VARIABLES
 
-people_in_queue = 0
+people_in_queue = 100
 waiting_time_in_queue = 0
 
-change_history = []
+new_people_histo = []
 
 #  UTILS
 
-def cap_history_at(max):
-    while len(change_history) > max:
-        del change_history[0]
+def remove_old_enter_records(time_before_now):
+    min_time = time() - time_before_now
+    for record_index in range(len(new_people_histo)-1, -1, -1):
+        if new_people_histo[record_index] >= min_time: continue
+        del new_people_histo[record_people_change]
 
 def record_people_change(kind, record_time = time()):
     global people_in_queue
     people_in_queue += 1 if kind == CHANGE_ENTER else -1
+
+    if kind == CHANGE_LEAVE: return people_in_queue
     
-    change_history.append((kind, record_time))
-    cap_history_at(MAX_HISTORY_LENGTH)
+    new_people_histo.append(record_time)
+    remove_old_enter_records(MAX_RECORD_LIVING_TIME_IN_SEC)
     return people_in_queue
 
 def seconds_to_minutes(seconds):
@@ -35,36 +40,16 @@ def seconds_to_minutes(seconds):
 
 #  MODEL
 
-def find_amount_of_useless_minutes(change_history):
-    active_people = 0
-    total_useless_mins = 0
-
-    for record_index in range(len(change_history)):
-        record = change_history[record_index]
-
-        if active_people == 0 and record_index > 0:
-            prev_record = change_history[record_index-1]
-            total_useless_mins += record[1] - prev_record[1]
-        
-        if record[0] == CHANGE_ENTER:   active_people += 1
-        elif record[0] == CHANGE_LEAVE: active_people -= 1
-
-    return total_useless_mins
-
-def history_to_growth_factor(kind, only_valuable = False):
-    change_history.sort(key=lambda v: v[1])
-    recording_start = change_history[0][1]
+def history_to_growth_factor():
+    new_people_histo.sort()
+    recording_start = new_people_histo[0]
     recorded_time = max(time() - recording_start, 1)
-    if only_valuable:
-        recorded_time -= find_amount_of_useless_minutes(change_history)
-    amount_of_matches = len(list(filter(lambda v: v[0] == kind, change_history)))
-    return amount_of_matches / seconds_to_minutes(recorded_time)
+    return len(new_people_histo) / seconds_to_minutes(recorded_time)
 
 def calc_model_values():
-    if len(change_history) == 0: return (0, 0, 0, 0)
-    I = history_to_growth_factor(CHANGE_ENTER)
-    M = history_to_growth_factor(CHANGE_LEAVE, only_valuable=True)
-    N = I / (M-I) if M != I and M-I != 0 else 0
+    if len(new_people_histo) == 0: return (0, 0, 0, 0)
+    I = history_to_growth_factor()
+    N = I / (M-I) if M-I != 0 else 0
     T = N / I if I != 0 else 0
     S = 1 / M if M != 0 else 0
     W = T - S
