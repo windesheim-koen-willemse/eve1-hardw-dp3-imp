2677d1ddff20364b8b980cc8f44fa5563f6a750e
Koen Willemse <s1212119@student.windesheim.nl>
2024-12-31 12:41:14 +0100

    1e testopstelling

--- states.py
+++ states.py
@@ -20,6 +20,7 @@ def transitions_to(possible_transitions):
     global active_state
     for target_state, condition in possible_transitions:
         if not condition: continue
+        print("[from state {} to state {}]".format(active_state, target_state))
         active_state = target_state
     pass
 
@@ -40,24 +41,28 @@ def active_state_to_icon():
 
 def handle_empty(is_trigger, people, time):
     if is_trigger: send_event_message(MESSAGE_CRITICAL, 'leeg')
+    if is_trigger: print("[state empty]")
     transitions_to([
         (STATE_CALM, people > 0)
     ])
 
 def handle_calm(is_trigger, people, time):
     if is_trigger: send_event_message(MESSAGE_WARNING, 'rustig')
+    if is_trigger: print("[state calm]")
     transitions_to([
         (STATE_EMPTY, people == 0),
         (STATE_NORMAL, people > 30 and time > 5)
     ])
 
 def handle_normal(is_trigger, people, time):
+    if is_trigger: print("[state normal]")
     transitions_to([
         (STATE_CALM, people <= 30 or time <= 5),
         (STATE_BUSY, people >= 130 or time > 30) # Corrected
     ])
 
 def handle_busy(is_trigger, people, time):
+    if is_trigger: print("[state busy]")
     if is_trigger: send_event_message(MESSAGE_WARNING, 'druk')
     transitions_to([
         (STATE_NORMAL, people < 130 and time <= 30), # Corrected
@@ -65,6 +70,7 @@ def handle_busy(is_trigger, people, time):
     ])
 
 def handle_full(is_trigger, people, time):
+    if is_trigger: print("[state full]")
     if is_trigger: send_event_message(MESSAGE_CRITICAL, 'vol')
     transitions_to([
         (STATE_BUSY, people < 160)
